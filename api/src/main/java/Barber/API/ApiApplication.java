2c9c484895b493c6cc8bf8649b612c4314507fd0
eg1to <pedro.h.egito@gmail.com>
2026-05-18 13:53:49 -0300

    Format code in ApiApplication for consistency

--- api/src/main/java/Barber/API/ApiApplication.java
+++ api/src/main/java/Barber/API/ApiApplication.java
@@ -1,4 +1,4 @@
-package Barber.API;
+package Barber.API; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
